b12d45b7a33cf9735cecd6fb99f25c2aec55840c
huxiaoou <516984451@qq.com>
2024-12-30 20:29:56 +0800

    Using both qtools.so and thostmduserapi_se.so

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 #include "QAccount.h"
-// #include "QOperator.h"
-// #include "QListener.h"
-// #include "QAnalyst.h"
+#include "QOperator.h"
+#include "QListener.h"
+#include "QAnalyst.h"
 
 int main()
 {
     QUtility::Account *account = new QUtility::Account("/home/ubuntu/deploy/account_md");
     account->Display();
 
-    // QUtility::QOperatorMsg *operatorMsg = new QUtility::QOperatorMsg("OperatorMsg");
-    // QUtility::QListenerMd *listenerMd = new QUtility::QListenerMd(operatorMsg);
-    // QUtility::QAnalyst *analyst = new QUtility::QAnalyst(operatorMsg, listenerMd);
-    // analyst->init();
-    // analyst->thread_main();
+    QUtility::QOperatorMsg *operatorMsg = new QUtility::QOperatorMsg("OperatorMsg");
+    QUtility::QListenerMd *listenerMd = new QUtility::QListenerMd(operatorMsg);
+    QUtility::QAnalyst *analyst = new QUtility::QAnalyst(operatorMsg, listenerMd);
+    analyst->init();
+    analyst->thread_main();
 }

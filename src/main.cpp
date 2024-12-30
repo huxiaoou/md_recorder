4fd1448eeeb23966fe8e5e6ee86af095bef3e55d
huxiaoou <516984451@qq.com>
2024-12-30 17:31:42 +0800

    Introduce library made by myself;Using CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-#include "QOperator.h"
-#include "QListener.h"
-#include "QAnalyst.h"
+#include "QAccount.h"
+// #include "QOperator.h"
+// #include "QListener.h"
+// #include "QAnalyst.h"
 
 int main()
 {
-    QUtility::QOperatorMsg *operatorMsg = new QUtility::QOperatorMsg("OperatorMsg");
-    QUtility::QListenerMd *listenerMd = new QUtility::QListenerMd(operatorMsg);
-    QUtility::QAnalyst *analyst = new QUtility::QAnalyst(operatorMsg, listenerMd);
-    analyst->init();
-    analyst->thread_main();
+    QUtility::Account *account = new QUtility::Account("/home/ubuntu/deploy/account_md");
+    account->Display();
+
+    // QUtility::QOperatorMsg *operatorMsg = new QUtility::QOperatorMsg("OperatorMsg");
+    // QUtility::QListenerMd *listenerMd = new QUtility::QListenerMd(operatorMsg);
+    // QUtility::QAnalyst *analyst = new QUtility::QAnalyst(operatorMsg, listenerMd);
+    // analyst->init();
+    // analyst->thread_main();
 }

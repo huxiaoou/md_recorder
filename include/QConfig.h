2d2ba6572554b049792e2428eaf75469e7aa7fa7
huxiaoou <516984451@qq.com>
2025-01-02 15:53:23 +0800

    Add calendarPath to config

--- include/QConfig.h
+++ include/QConfig.h
@@ -9,13 +9,15 @@ namespace QUtility
     class ConfigMd
     {
     private:
+        char _calendarPath[240];
         char _accountFilePath[240];
         char _mdSaveRootDir[240];
         char _subscribedFile[240];
 
     public:
-        ConfigMd(const char * config_path);
+        ConfigMd(const char *config_path);
         unsigned match(const char *text_line);
+        const char *GetCalendarPath() const { return _calendarPath; }
         const char *GetAccountFilePath() const { return _accountFilePath; }
         const char *GetMdSaveRootDir() const { return _mdSaveRootDir; }
         const char *GetSubscribedFile() const { return _subscribedFile; }
--- src/QConfig.cpp
+++ src/QConfig.cpp
@@ -14,7 +14,7 @@ namespace QUtility
             res += match(text_line);
         }
         display();
-        if (res != 7)
+        if (res != 15) // 15 = 1 + 2 + 4 + 8
         {
             std::cout << "Failed to initialize configuration" << std::endl;
             std::cout << "Please check again" << std::endl;
@@ -25,6 +25,7 @@ namespace QUtility
     void ConfigMd::display() const
     {
         std::cout << "MD CONFIG SUMMARY:" << std::endl;
+        std::cout << "------CALENDAR PATH    :" << _calendarPath << std::endl;
         std::cout << "------ACCOUNT FILE PATH:" << _accountFilePath << std::endl;
         std::cout << "------MD SAVE ROOT DIR :" << _mdSaveRootDir << std::endl;
         std::cout << "------SUBSCRIBED FILE  :" << _subscribedFile << std::endl;
@@ -32,23 +33,28 @@ namespace QUtility
 
     unsigned ConfigMd::match(const char *text_line)
     {
-        char var_name[240];
-        char var_val[240];
-        sscanf(text_line, "%[^=]=%s", var_name, var_val);
-        if (std::strcmp(var_name, "accountFilePath") == 0)
+        char key[240];
+        char val[240];
+        sscanf(text_line, "%[^=]=%s", key, val);
+        if (std::strcmp(key, "calendarPath") == 0)
         {
-            strcpy(_accountFilePath, var_val);
+            strcpy(_calendarPath, val);
             return 1;
         }
-        if (std::strcmp(var_name, "mdSaveRootDir") == 0)
+        if (std::strcmp(key, "accountFilePath") == 0)
         {
-            strcpy(_mdSaveRootDir, var_val);
+            strcpy(_accountFilePath, val);
             return 2;
         }
-        if (std::strcmp(var_name, "subscribedFile") == 0)
+        if (std::strcmp(key, "mdSaveRootDir") == 0)
         {
-            strcpy(_subscribedFile, var_val);
+            strcpy(_mdSaveRootDir, val);
             return 4;
+        }
+        if (std::strcmp(key, "subscribedFile") == 0)
+        {
+            strcpy(_subscribedFile, val);
+            return 8;
         }
         return 0;
     }
